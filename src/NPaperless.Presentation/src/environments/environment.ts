cf4810bbc5f653895854fe324747022e965fb020
Daniel Hofbauer <hofbauer.d@icloud.com>
2023-10-13 12:17:28 +0200

    commit

--- src/NPaperless.Presentation/src/environments/environment.ts
+++ src/NPaperless.Presentation/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000/api/',
+  apiBaseUrl: 'http://localhost:8080/api/',
   apiVersion: '3',
   appTitle: 'Paperless-ngx',
   version: 'DEVELOPMENT',
-  webSocketHost: 'localhost:8000',
+  webSocketHost: 'localhost:8080',
   webSocketProtocol: 'ws:',
   webSocketBaseUrl: '/ws/',
 }
